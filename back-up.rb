5ec2ef4a3a95392782be6585c8d2ca0c42eda119
Matthew Britt <mwbritt@hotmail.com>
2019-12-03 17:21:29 +0000

    Done.

--- back-up.rb
+++ back-up.rb
@@ -15,9 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-  
-  programmer_hash[:grace_hopper]
-  
+programmer_hash[:grace_hopper]
+
 end
 
 def alan_kay_is_known_for
@@ -38,9 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-     
-  programmer_hash[:alan_kay][:known_for]
-   
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -59,9 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     
+
   programmer_hash[:dennis_ritchie][:languages][0]
-end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -85,12 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-  programmer_hash[:yukihiro_matsumoto] = {}
-  programmer_hash[:yukihiro_matsumoto][:known_for] = "Front-end wizardry"
-  programmer_hash[:yukihiro_matsumoto][:languages] = ["javascript", "php"]
-  programmer_hash
-    
+ 
 end
 
 def changing_alan
@@ -112,9 +103,6 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
-     programmer_hash
-     
      
 end
 
@@ -134,8 +122,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     
-  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
-  programmer_hash
 
 end
